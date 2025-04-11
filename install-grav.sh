b98c30a7dbf45d039f54073a4c942c6b0cc73d01
jmdeleon <jmdeleon@users.noreply.github.com>
2025-04-11 11:21:40 +0000

    fix:Use git install method.

--- install-grav.sh
+++ install-grav.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 mkdir grav
 cd grav
+git clone -b master https://github.com/getgrav/grav.git .
 ddev config --omit-containers=db
 ddev start
-ddev composer create getgrav/grav
+ddev composer install
+ddev exec grav install
 ddev exec gpm install admin -y
 ddev launch
